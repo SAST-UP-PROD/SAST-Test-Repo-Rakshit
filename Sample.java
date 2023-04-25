76f8186d6a6050132201ef9b0955c44d780d52a5
Rakshit Krishnappa Ravi <101260874+rakshitkravi@users.noreply.github.com>
2023-04-25 13:32:46 +0200

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.owasp.encoder.Encode;
 
+
 @RestController
 @RequestMapping("CWE79")
 public class CWE79 {
